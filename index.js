ce49b93cf8cb381b9285b69331d155321a59bc90
loicbaecher <74916339+loicbaecher@users.noreplay.github.com>
2023-03-15 10:22:36 +0100

    user can change the grid number but not functionnal yet
    An editable text location and a button has been added. The functionnali-
    ties have yet to be built.

--- index.js
+++ index.js
@@ -11,9 +11,18 @@ window.addEventListener("mousemove",(e)=>{
     const x=e.clientX;
     const y=e.clientY;
 
+    //replace background style with changes in the gradien
     body.style=`background: rgb(251,255,177);
     background: radial-gradient(circle at ${x}px ${y}px, rgba(251,255,177,1) 0%, rgba(255,191,169,1) 75%, rgba(255,172,172,1) 100%) fixed;
     font-family: 'Josefin Sans', sans-serif;`
 })
 
-//function to get the degrees of the mouse relative to the screen
+const etch=document.createElement("div",);
+etch.classList.add("case");
+bigcont=document.querySelector(".bigcontainer");
+bigcont.appendChild(etch);
+
+//gridbut=document.querySelector("button.rules")
+//gridbut.addEventListener('click',function(e){
+    
+//})
